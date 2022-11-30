98115dff4e46c27a8fa95358a5cb58ff57accf41
manishaagarwal1311 <112688958+manishaagarwal1311@users.noreply.github.com>
2022-11-30 12:19:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,28 @@
-terraform {
-  required_providers {
-    powermax = {
-       version = "0.0.1"
-      source  = "dell/powermax"
-    }
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-provider "powermax" {
-  username = var.username
-  password = var.password
-  endpoint = var.endpoint
-  serial_number = var.serial_number
-  insecure = true
-}
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
 
-resource "powermax_storage_group" "storage_group_2" {
-	name = "sg_tf_cloud"
-	srpid = "SRP_1"
-	service_level = "Diamond"
+  tags = {
+    Name = var.instance_name
+  }
 }
